6a776103fceca308aa8be50d23ce9e19437400da
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-05-08 15:07:02 +0500

    Add files via upload

---
+++ Code Forces/Magnets.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	
+	for(int i = 0 ; i < n ; i++){
+		cin>>arr[i];
+	}
+	
+	int groups = 0;
+	for(int i = 0 ; i < n-1 ; i++){
+		if(arr[i] != arr[i+1]){
+			groups++;
+		}
+	}
+	cout<<groups<<endl;
+	
+	return 0;
+}
