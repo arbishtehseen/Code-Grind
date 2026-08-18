34d7cb26df0caefdfa66b0a8a4eeda18ab9c96bb
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-08-18 23:02:22 +0500

    Add files via upload

---
+++ Code Forces/Odd one out.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	
+	while(t--){
+		int a , b , c;
+		cin>>a>>b>>c;
+		
+		if(a == b || a == c){
+			if( a == b){
+				cout<<c<<endl;
+			}
+			else{
+				cout<<b<<endl;
+			}
+		}
+		else{
+			cout<<a<<endl;
+		}
+	}
+	
+	
+	return 0;
+}
