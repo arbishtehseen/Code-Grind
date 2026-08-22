135baa3fe81576f3e68feab5b2449062f7c7db5c
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-08-22 19:50:41 +0500

    Add files via upload

---
+++ C++ STL/Iterator to vector of pairs..cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	vector<pair<int,int>> v_p = { {1,2} , {2,3} , {3,4} };
+	vector<pair<int,int>> :: iterator it;
+	// iterator of vector of pairs.
+	for(it = v_p.begin() ; it != v_p.end() ; ++it){
+		cout<< (*it).first <<" "<< (*it).second << endl;
+	}
+	// here (*it) is a pair.
+	
+	return 0;
+}
